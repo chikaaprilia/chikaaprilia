49d9d14fd73d81aa8b94ddcf5aa4211a07b205ff
chika <chikaaprilia245@gmail.com>
2024-02-03 10:20:57 +0700

    menambahkan file index.html

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layout')
+@section('content')
+ <div class="card">
+    <div class="card-body">
+        <h3>Selamat Datang Di Halaman Dashboard Admin.</h3>
+    </div>
+ </div>
+
+@endsection
